c5e9e47b00d812e5e1fa48ae3e0c1b00f8434ea6
unknown <ansarit467@gmail.com>
2021-02-14 10:59:42 +0530

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +140,6 @@ STATICFILES_DIRS = [
             ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
